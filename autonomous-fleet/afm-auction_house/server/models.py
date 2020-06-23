fba26e077ccbea8706258e837a7b5c464faf21c7
PsyMadDoc <39226366+PsyMadDoc@users.noreply.github.com>
2020-06-23 09:34:09 +0300

    added backend data model

---
+++ autonomous-fleet/afm-auction_house/server/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+class Text(models.Model):
+    headline = models.CharField(max_length=100, blank=True, null=True)
+    body_text = models.TextField(blank=True, null=True)
+    pub_date = models.DateField('publishing date')
+
+    def __str__(self):
+        return self.headline
