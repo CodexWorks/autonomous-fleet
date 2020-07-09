bce069d5730dbba4720bd7e821bea3dab26e86a3
PsyMadDoc <39226366+PsyMadDoc@users.noreply.github.com>
2020-07-09 14:16:14 +0300

    added registration method

--- autonomous-fleet/afm-ui/src/store/actions/auth.js
+++ autonomous-fleet/afm-ui/src/store/actions/auth.js
@@ -50,9 +50,33 @@ export const authLogin = (username, password) => {
   return (dispatch) => {
     dispatch(authStart());
     axios
-      .post('http://127.0.0.1:8000/rest-auth/login', {
+      .post('http://127.0.0.1:8000/rest-auth/login/', {
         username: username,
         password: password,
+      })
+      .then((res) => {
+        const token = res.data.key;
+        const expirationDate = new Date(new Date().getTime() + 3600 * 1000); // 3600 seconds * 1000 = 1 hour
+        localStorage.setItem('token', token); // sets token in browser local memory
+        localStorage.setItem('expirationDate', expirationDate); // sets expiration date in browser local memory
+        dispatch(authSuccess(token));
+        dispatch(checkAuthTimeout(3600)); // 1 hour
+      })
+      .catch((error) => {
+        dispatch(authFail(error));
+      });
+  };
+};
+
+export const authSignup = (username, email, password1, password2) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    axios
+      .post('http://127.0.0.1:8000/rest-auth/registration/', {
+        username: username,
+        email: email,
+        password1: password1,
+        password1: password2,
       })
       .then((res) => {
         const token = res.data.key;
