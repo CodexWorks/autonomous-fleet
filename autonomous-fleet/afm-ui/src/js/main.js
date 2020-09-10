a84e0d7e1387470f29484e41fd83e8b62f39b63d
PsyMadDoc <39226366+PsyMadDoc@users.noreply.github.com>
2020-09-10 14:48:55 +0300

    commented bug

--- autonomous-fleet/afm-ui/src/js/main.js
+++ autonomous-fleet/afm-ui/src/js/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 // Applies 'active' class to sidebar elems when toggle btn is clicked
+// Deprecated - add js method onClick() in MainNavBar
 
 $(document).ready(function () {
   $('#sidebarCollapse').on('click', function () {
